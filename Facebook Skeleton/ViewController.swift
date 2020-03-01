2f68642beaef5d70df4687d35dee41a9e663ed21
Michael Grant <mdgrant116@gmail.com>
2020-03-01 10:20:29 -0500

    Initial Commit

---
+++ Facebook Skeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facebook Skeleton
+//
+//  Created by Michael Grant on 3/1/20.
+//  Copyright © 2020 Michael Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
