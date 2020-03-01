4dbca25103bee6da3ba0a3f72990de0fcaa63cd8
Michael Grant <mdgrant116@gmail.com>
2020-03-01 10:29:15 -0500

    Initial setup complete

--- Facebook Skeleton/ViewController.swift
+++ Facebook Skeleton/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showSkeleton()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func showSkeleton() {
+        let skeletonView = UIView()
+        
+        skeletonView.backgroundColor = .white
+        view.addSubview(skeletonView)
+        skeletonView.frame = CGRect(x: 0, y: 10, width: view.bounds.width, height: 250)
+        
+    }
+    
 }
 
