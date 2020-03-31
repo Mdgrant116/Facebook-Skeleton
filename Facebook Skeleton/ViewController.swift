425cbb30c63716b0ea766988aa2a3e934674049c
Michael Grant <michael.grand@meijer.com>
2020-03-31 09:59:37 -0400

    Skeleton laid out

--- Facebook Skeleton/ViewController.swift
+++ Facebook Skeleton/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        showSkeleton()
+        showSkeleton1()
+        showSkeleton2()
     }
 
     override func viewDidLoad() {
@@ -25,48 +26,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    func showSkeleton() {
+    func showSkeleton1() {
         let skeletonView = UIView()
 
         skeletonView.backgroundColor = .white
        
         view.addSubview(skeletonView)
 
-       
-//        let numberOfCells = Int(viewHeight / 150 + 1)
-       
 
-        skeletonView.frame = CGRect(x: 10, y: 10, width: view.bounds.width - 20, height: 150)
+        skeletonView.frame = CGRect(x: 10, y: 18, width: view.bounds.width - 20, height: 80)
         
-        
+       
         
         let backgroundGradientLayer = CAGradientLayer()
 
         backgroundGradientLayer.frame = skeletonView.bounds
-
         backgroundGradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
         backgroundGradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-
         backgroundGradientLayer.locations = [0.25, 0.5, 0.75]
 
         backgroundGradientLayer.colors = [
-            UIColor(red: 184 / 255, green: 184 / 255, blue: 184 / 255, alpha: 1.0).cgColor,
-            UIColor(red: 211 / 255, green: 211 / 255, blue: 211 / 255, alpha: 1.0).cgColor,
-            UIColor(red: 184 / 255, green: 184 / 255, blue: 184 / 255, alpha: 1.0).cgColor,
+            UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.23).cgColor,
+            UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.04).cgColor,
+            UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.23).cgColor
         ]
 
         skeletonView.layer.addSublayer(backgroundGradientLayer)
+//        skeletonView2.layer.addSublayer(backgroundGradientLayer)
+        
         let gradientAnimation = CABasicAnimation(keyPath: "locations")
-
         gradientAnimation.fromValue = [0.0, 0.0, 0.25]
         gradientAnimation.toValue = [0.65, 1.0, 1.0]
-
-        gradientAnimation.duration = 1.5
-
+        gradientAnimation.duration = 1.7
         gradientAnimation.repeatCount = .greatestFiniteMagnitude
         gradientAnimation.fillMode = CAMediaTimingFillMode.forwards
         gradientAnimation.isRemovedOnCompletion = false
-
         backgroundGradientLayer.add(gradientAnimation, forKey: nil)
 
         let skeletonFrame = UIBezierPath()
@@ -77,33 +71,27 @@ class ViewController: UIViewController {
         skeletonView.layer.shadowOffset = CGSize(width: 0, height: 2)
         skeletonView.layer.shadowRadius = 4
         
-        
-        let total = UILabel()
-        total.font = UIFont(name: "SFProText", size: 90)
-        total.textColor = #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1)
-        total.text = "MEIJER SAVINGS"
-        total.frame = CGRect(x: 20, y: 10, width: 200, height: 20)
-        skeletonView.addSubview(total)
-        
+//        let total = UILabel()
+//        total.font = UIFont(name: "SFProText", size: 90)
+//        total.textColor = #colorLiteral(red: 0.2901960784, green: 0.5647058824, blue: 0.8862745098, alpha: 1)
+//        total.text = "MEIJER SAVINGS"
+//        total.frame = CGRect(x: 20, y: 10, width: 200, height: 20)
+//        skeletonView.addSubview(total)
+//
 
-        let contentFrame1 = UIBezierPath(rect: CGRect(x: 20, y: 60 , width: view.bounds.width * 0.65, height: 10))
-
+        let contentFrame1 = UIBezierPath(roundedRect: CGRect(x: 20, y: 15 , width: view.bounds.width * 0.40, height: 20), cornerRadius: 5)
         skeletonFrame.append(contentFrame1)
-
-        let contentFrame2 = UIBezierPath(rect: CGRect(x: 20, y: 95 , width: view.bounds.width * 0.75, height: 1))
-        skeletonFrame.append(contentFrame2)
-
-        let contenFrame3 = UIBezierPath(rect: CGRect(x: 20, y: 140, width: view.bounds.width * 0.4, height: 8))
-
-        skeletonFrame.append(contenFrame3)
-    
+        
+        let contentFrame2 = UIBezierPath(roundedRect: CGRect(x: 20, y: 50 , width: view.bounds.width * 0.45, height: 20), cornerRadius: 5)
+            skeletonFrame.append(contentFrame2)
+        
+        let contentFrame3 = UIBezierPath(roundedRect: CGRect(x: 265, y: 50 , width: view.bounds.width * 0.20, height: 20), cornerRadius: 5)
+        skeletonFrame.append(contentFrame3)
+        
+        
+     
         
         button.layer.cornerRadius = 15
-
-//        let gutter = UIView(frame: CGRect(x: 0, y: (150 - gutterHeight), width: view.bounds.width, height: gutterHeight))
-//
-//        gutter.backgroundColor = UIColor(red: 216 / 255, green: 216 / 255, blue: 216 / 255, alpha: 1)
-//        skeletonView.addSubview(gutter)
 
 
       
@@ -115,6 +103,90 @@ class ViewController: UIViewController {
         skeltonShape.fillColor = UIColor.black.cgColor
 
         skeltonShape.frame = skeletonView.bounds
+
+        backgroundGradientLayer.mask = skeltonShape
+    }
+    
+    func showSkeleton2() {
+        
+        let skeletonView2 = UIView()
+               skeletonView2.backgroundColor = .white
+               view.addSubview(skeletonView2)
+               skeletonView2.frame = CGRect(x: 10, y: 110, width: view.bounds.width - 20, height: 228)
+        
+           
+                let backgroundGradientLayer = CAGradientLayer()
+
+                backgroundGradientLayer.frame = skeletonView2.bounds
+                backgroundGradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+                backgroundGradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+                backgroundGradientLayer.locations = [0.25, 0.5, 0.75]
+
+                backgroundGradientLayer.colors = [
+                    UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.23).cgColor,
+                    UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.04).cgColor,
+                    UIColor(red: 42 / 255, green: 46 / 255, blue: 48 / 255, alpha: 0.23).cgColor
+                ]
+
+                skeletonView2.layer.addSublayer(backgroundGradientLayer)
+                
+                let gradientAnimation = CABasicAnimation(keyPath: "locations")
+                gradientAnimation.fromValue = [0.0, 0.0, 0.25]
+                gradientAnimation.toValue = [0.65, 1.0, 1.0]
+                gradientAnimation.duration = 1.7
+                gradientAnimation.repeatCount = .greatestFiniteMagnitude
+                gradientAnimation.fillMode = CAMediaTimingFillMode.forwards
+                gradientAnimation.isRemovedOnCompletion = false
+                backgroundGradientLayer.add(gradientAnimation, forKey: nil)
+
+                let skeletonFrame = UIBezierPath()
+          
+                skeletonView2.layer.cornerRadius = 15
+                skeletonView2.layer.shadowOpacity = 1
+                skeletonView2.layer.shadowColor = #colorLiteral(red: 0.8823529412, green: 0.8823529412, blue: 0.8823529412, alpha: 1)
+                skeletonView2.layer.shadowOffset = CGSize(width: 0, height: 2)
+                skeletonView2.layer.shadowRadius = 4
+                
+        
+        
+        let contentFrame1 = UIBezierPath(roundedRect: CGRect(x: 20, y: 15 , width: view.bounds.width * 0.40, height: 20), cornerRadius: 5)
+              skeletonFrame.append(contentFrame1)
+              
+              let contentFrame2 = UIBezierPath(roundedRect: CGRect(x: 20, y: 50 , width: view.bounds.width * 0.45, height: 20), cornerRadius: 5)
+                  skeletonFrame.append(contentFrame2)
+              
+              let contentFrame3 = UIBezierPath(roundedRect: CGRect(x: 265, y: 50 , width: view.bounds.width * 0.20, height: 20), cornerRadius: 5)
+              skeletonFrame.append(contentFrame3)
+        
+        
+        
+        let contentFrame4 = UIBezierPath(roundedRect: CGRect(x: 20, y: 100 , width: view.bounds.width * 0.45, height: 20), cornerRadius: 5)
+            skeletonFrame.append(contentFrame4)
+        
+        let contentFrame5 = UIBezierPath(roundedRect: CGRect(x: 265, y: 100 , width: view.bounds.width * 0.20, height: 20), cornerRadius: 5)
+        skeletonFrame.append(contentFrame5)
+//
+        let contentFrame6 = UIBezierPath(roundedRect: CGRect(x: 20, y: 150 , width: view.bounds.width * 0.45, height: 20), cornerRadius: 5)
+            skeletonFrame.append(contentFrame6)
+
+        let contentFrame7 = UIBezierPath(roundedRect: CGRect(x: 265, y: 150 , width: view.bounds.width * 0.20, height: 20), cornerRadius: 5)
+        skeletonFrame.append(contentFrame7)
+//
+        let contentFrame8 = UIBezierPath(roundedRect: CGRect(x: 20, y: 200 , width: view.bounds.width * 0.45, height: 20), cornerRadius: 5)
+            skeletonFrame.append(contentFrame8)
+
+        let contentFrame9 = UIBezierPath(roundedRect: CGRect(x: 265, y: 200 , width: view.bounds.width * 0.20, height: 20), cornerRadius: 5)
+        skeletonFrame.append(contentFrame9)
+        
+        
+
+        let skeltonShape = CAShapeLayer()
+
+        skeltonShape.path = skeletonFrame.cgPath
+
+        skeltonShape.fillColor = UIColor.black.cgColor
+
+        skeltonShape.frame = skeletonView2.bounds
 
         backgroundGradientLayer.mask = skeltonShape
     }
